3b6d9815bc9e9cf6ef64fd9587be8e115f6a7da6
Cristian Hornedo <officialcristianl@gmail.com>
2020-06-29 18:26:55 -0400

    Made 4 functions which give the unicode of the lower, upper, numbers, and symbols which we can then use to generate our password later

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // ! WHEN I click the button to generate a password
 // ! THEN I am presented with a series of prompts for password criteria
-// * event 
+// * event
 // ! WHEN prompted for password criteria
 // ! THEN I select which criteria to include in the password
 // ! WHEN prompted for the length of the password
@@ -18,39 +18,66 @@
 var generateBtn = document.querySelector("#generate");
 
 // ? Write password to the #password input
+
+function getRanLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRanUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRanNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRanNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+
 function generatePassword() {
   // ! Length 8 - 128
   let len;
   do {
-    len = prompt("How many characters would you like to have in your password? (Between 8 - 128 characters)");
+    len = prompt(
+      "How many characters would you like to have in your password? (Between 8 - 128 characters)"
+    );
     len = parseInt(len);
-  } while (len <= 8 || len >= 128);
+  } while (len < 8 || len > 128);
 
   if (len >= 8 || len <= 128) {
     console.log("This is the len: " + len);
 
     // ! Ask for lowercase, uppercase, numeric, and special characters
+
+    alert("Please select AT LEAST one character type");
+
     let isLowerCase = confirm("do you want lowercase in your password?");
     console.log(isLowerCase);
-  
+
     let isUpperCase = confirm("do you want uppercase in your password?");
     console.log(isUpperCase);
-  
-    let isNumeric = confirm("do you want numbers in your password?")
+
+    let isNumeric = confirm("do you want numbers in your password?");
     console.log(isNumeric);
-  
-    let isSpecialCha = confirm("do you want special characters in your password?")
+
+    let isSpecialCha = confirm(
+      "do you want special characters in your password?"
+    );
     console.log(isSpecialCha);
-  
+
     // ! At least one character type should be selected for whatever gets accepted
-  
-  
+    
+
+    for (let i = 0; i <= len; i++) {
+
+    }
+    
+
     // ! return password to this function
   } else {
-    alert("Invalid input")
+    alert("Invalid input");
   }
-
-
 }
 
 function writePassword() {
@@ -58,7 +85,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // ? Add event listener to generate button
