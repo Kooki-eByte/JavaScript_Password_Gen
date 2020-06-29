d5f52de149c95e5505e3c7f92784747725513cd3
Cristian Hornedo <officialcristianl@gmail.com>
2020-06-29 15:15:52 -0400

    plugged in html to css and js files. Gave functionality to generate button to ask for length and the rest of the other questions and now working on proofing the prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,56 @@
-// Assignment Code
+// ! WHEN I click the button to generate a password
+// ! THEN I am presented with a series of prompts for password criteria
+// * event 
+// ! WHEN prompted for password criteria
+// ! THEN I select which criteria to include in the password
+// ! WHEN prompted for the length of the password
+// ! THEN I choose a length of at least 8 characters and no more than 128 characters
+// ! WHEN prompted for character types to include in the password
+// ! THEN I choose lowercase, uppercase, numeric, and/or special characters
+// ! WHEN I answer each prompt
+// ! THEN my input should be validated and at least one character type should be selected
+// ! WHEN all prompts are answered
+// ! THEN a password is generated that matches the selected criteria
+// ! WHEN the password is generated
+// ! THEN the password is either displayed in an confirm or written to the page
+
+// ! Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// ? Write password to the #password input
+function generatePassword() {
+  // ! Length 8 - 128
+  let len = prompt("How many characters would you like to have in your password? (Please use numeric)");
+  len = parseInt(len);
+
+  if (len >= 8 || len <= 128) {
+    console.log("This is the len: " + len);
+
+    // ! Ask for lowercase, uppercase, numeric, and special characters
+    let isLowerCase = confirm("do you want lowercase in your password?");
+    console.log(isLowerCase);
+  
+    let isUpperCase = confirm("do you want uppercase in your password?");
+    console.log(isUpperCase);
+  
+    let isNumeric = confirm("do you want numbers in your password?")
+    console.log(isNumeric);
+  
+    let isSpecialCha = confirm("do you want special characters in your password?")
+    console.log(isSpecialCha);
+  
+    // ! At least one character type should be selected for whatever gets accepted
+  
+  
+    // ! return password to this function
+  } else {
+    alert("Invalid input")
+    break;
+  }
+
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +59,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// ? Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScript_Password_Gen
 an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+## Comment Rules
+- ! Alert 
+- * Highlight 
+- ? Code to look over 
